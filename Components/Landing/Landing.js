8b1c43e9b4658911d7609059fd9a6d514a1ab043
shahnawaz <shahnawazgull2166@gmail.com>
2025-01-31 23:05:20 +0500

    homepage Errors

--- Components/Landing/Landing.js
+++ Components/Landing/Landing.js
@@ -68,7 +68,7 @@ const Landing = () => {
       <div className="old-new-way-section">
         <div className="old-way-wrapper">
           <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M45 20L55 30M55 30L45 40M55 30H5" stroke="#5356FF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M45 20L55 30M55 30L45 40M55 30H5" stroke="#5356FF" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
 
           <p className="old-new-way-section-heading">
@@ -94,7 +94,7 @@ const Landing = () => {
 
         <div className="old-way-wrapper">
           <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M45 20L55 30M55 30L45 40M55 30H5" stroke="#5356FF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M45 20L55 30M55 30L45 40M55 30H5" stroke="#5356FF" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
           <p className="old-new-way-section-heading">
             The <span> new way</span>: Streamlined Campaign Upload from Your
@@ -245,11 +245,11 @@ const Landing = () => {
         <p className="price-desc">Flexible Pricing to Suit Every Advertiser</p>
         <div className="price-card-container">
           <div className="price-card">
-            <div class="price-card-description-container">
-              <p class="price-card-price">$0</p>
-              <p class="price-card-accounts">1 Ad Account</p>
-              <p class="price-card-plan">Free 1 Day Trial</p>
-              <p class="price-card-plan-desc">Try QuickCampaigns Now Risk-Free</p>
+            <div className="price-card-description-container">
+              <p className="price-card-price">$0</p>
+              <p className="price-card-accounts">1 Ad Account</p>
+              <p className="price-card-plan">Free 1 Day Trial</p>
+              <p className="price-card-plan-desc">Try QuickCampaigns Now Risk-Free</p>
             </div>
             <div className="price-card-feature-container">
               <div className="price-card-feature text-free">
@@ -283,7 +283,7 @@ const Landing = () => {
                 <path d="M38.5158 9.90098C25.6315 17.6818 18.4779 31.9119 14.1785 45.8289C13.9575 46.5441 13.6873 47.2619 13.4404 47.9605C12.75 46.5676 8.03917 34.9998 6.13728 35.707C5.02576 36.1204 5.66158 37.6059 5.90619 38.2052C7.67675 42.5372 9.40645 46.4209 11.522 50.6102C13.1143 53.7639 16.0773 52.9418 19.0123 51.9819C23.4934 50.5165 23.8571 50.0963 28.4466 48.4097C29.203 48.1315 29.9979 47.9067 30.699 47.5593C31.5233 47.1502 32.4071 46.2845 31.9695 45.341C31.4735 44.2716 30.3945 44.3471 29.4728 44.7397C29.2803 44.8222 29.0849 44.8977 28.8877 44.9686C26.7215 45.7463 16.8895 49.8951 16.562 49.1937C16.4515 48.9575 16.4888 48.6827 16.5306 48.4255C17.7342 41.0033 20.9214 33.9331 24.6866 27.4731C27.2199 23.126 30.4713 19.1994 34.2601 15.8874C37.3047 13.2258 40.7371 10.966 44.4762 9.40628C48.4857 7.73348 52.366 7.38881 56.6492 7.13439C59.1151 6.98817 57.162 4.63891 55.7901 4.37429C54.2823 4.08303 52.668 4.60609 51.2031 4.91103C46.5692 5.87811 42.3375 7.59325 38.5158 9.90098Z" fill="#5356FF" />
               </svg>
             </div>
-            <div class="price-card-description-container">
+            <div className="price-card-description-container">
               <p className="price-card-price">$129.95/month</p>
               <p className="price-card-accounts">1 Ad Account</p>
               <p className="price-card-plan">Professional Plan</p>
